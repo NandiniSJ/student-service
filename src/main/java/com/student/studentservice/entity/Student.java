b3968164e0c0b49bad2891ce2b6947e1dac935b5
NandiniSJ <nandinisjadhav97@gmail.com>
2023-03-13 14:12:57 +0530

    Initial commit

---
+++ src/main/java/com/student/studentservice/entity/Student.java
@@ -0,0 +1,14 @@
+package com.student.studentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity(name = "students")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+}
